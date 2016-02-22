0083987f9e7c93bc6f26b44ad36e6fce72612531
jdavid05 <joeh.davidson@gmail.com>
2016-02-22 18:35:50 -0400

    Added IssueTracker files

---
+++ IssueTracker/Program.cs
@@ -0,0 +1,93 @@
+﻿/* Purpose: Manage the attributes of an issue with 
+ *          city infrastructure for Happyville, Nova Scotia
+ * Author : Joseph Davidson
+ * Date   : February, 2016
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace IssueTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test the initial constructor.
+            Tracker tracker1 = new Tracker(2, 3, 3, "It's Bad", "Some", "Body", Convert.ToDateTime("2016-02-02"));
+            Console.Write("Returned: ");
+            Console.WriteLine(tracker1.GetIssueDetails(true));
+            Console.WriteLine("Expected: 1, {X=2,Y=3}, road, It's Bad, Some Body, 2/2/2016");
+
+            //Print a blank line for formatting.
+            Console.WriteLine();
+
+            // Test the second constructor.
+            Tracker tracker2 = new Tracker(2, 2, 2, "Broken");
+            Console.Write("Returned: ");
+            Console.WriteLine(tracker2.GetIssueDetails(true));
+            Console.WriteLine("Expected: 2, {X=2,Y=2}, signage, Broken, Not Known, 2/7/2016");
+
+            //Print a blank line for formatting.
+            Console.WriteLine();
+
+            // Test the Issue Date for dates in the future.
+            Tracker tracker3 = new Tracker(0, 0, 0, "", "", "", Convert.ToDateTime("2016-03-01"));
+            Console.Write("Returned: ");
+            Console.WriteLine(tracker3.IssueDate);
+            Console.WriteLine("Expected: 2/7/2016 12:00:00 AM");
+
+            //Print a blank line for formatting.
+            Console.WriteLine();
+
+            // Test the Issue Date for dates > than 1 year ago.
+            Tracker tracker4 = new Tracker(0, 0, 0, "", "", "", Convert.ToDateTime("2015-02-01"));
+            Console.Write("Returned: ");
+            Console.WriteLine(tracker4.IssueDate);
+            Console.WriteLine("Expected: 2/7/2016 12:00:00 AM");
+
+            //Print a blank line for formatting.
+            Console.WriteLine();
+
+            // Test the Issue Type method for the 'facility' Issue Type.
+            Tracker tracker5 = new Tracker(0, 0, 1, "");
+            Console.Write("Returned: ");
+            Console.WriteLine(tracker5.GetIssueType());
+            Console.WriteLine("Expected: facility");
+
+            //Print a blank line for formatting.
+            Console.WriteLine();
+
+            // Test the Issue Type method for the 'facility' Issue Type.
+            Tracker tracker6 = new Tracker(0, 0, 2, "");
+            Console.Write("Returned: ");
+            Console.WriteLine(tracker6.GetIssueType());
+            Console.WriteLine("Expected: signage");
+
+            //Print a blank line for formatting.
+            Console.WriteLine();
+
+            // Test the Issue Type method for the 'facility' Issue Type.
+            Tracker tracker7 = new Tracker(0, 0, 3, "");
+            Console.Write("Returned: ");
+            Console.WriteLine(tracker7.GetIssueType());
+            Console.WriteLine("Expected: road");
+
+            //Print a blank line for formatting.
+            Console.WriteLine();
+
+            // Test the Issue Type method for numbers outside of 1, 2, or 3.
+            Tracker tracker8 = new Tracker(0, 0, 5, "");
+            Console.Write("Returned: ");
+            Console.WriteLine(tracker8.GetIssueType());
+            Console.WriteLine("Expected: unknown");
+
+            // Pause the program
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IssueTracker/Tracker.cs
@@ -0,0 +1,153 @@
+﻿/* Purpose: Manage the attributes of an issue with 
+ *          city infrastructure for Happyville, Nova Scotia
+ * Author : Joseph Davidson
+ * Date   : February, 2016
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace IssueTracker
+{
+    class Tracker
+    {
+        #region Fields
+        /// <summary>
+        /// Fields
+        /// </summary>
+        public static int CurrentID = 1;
+        private Point issueLocation;
+        private DateTime issueDate;
+        private int issueID;
+        private int issueType;
+        private string firstName, lastName;
+        private string issueDescription;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Properties
+        /// </summary>
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public DateTime IssueDate 
+        { 
+            get { return issueDate; } 
+            set 
+            { 
+                int diffYear = (int)(DateTime.Today - value).TotalDays;
+                if ((diffYear < 0 || diffYear > 365))
+                    issueDate = DateTime.Today;
+                else issueDate = value; 
+            } 
+        }
+        public int IssueType 
+        { 
+            get { return issueType; } 
+            set 
+            { 
+                if (!(value == 1 || value == 2 || value == 3)) 
+                    issueType = 0;
+                else issueType = value;
+            } 
+        }
+        #endregion
+
+        #region Initial Constructor
+        /// <summary>
+        /// Initial Constructor -- All values declared
+        /// </summary>
+        /// <param name="coordinateX">Point Value X</param>
+        /// <param name="coordinateY">Point Value Y</param>
+        /// <param name="it">Issue Type</param>
+        /// <param name="id">Issue Description</param>
+        /// <param name="fn">First Name</param>
+        /// <param name="ln">Last Name</param>
+        /// <param name="idate">Issue Date</param>
+        public Tracker(int coordinateX, int coordinateY, int it, string id, string fn, string ln, DateTime iDate)
+        {
+            issueLocation = new Point(coordinateX, coordinateY);
+            FirstName = fn;
+            LastName = ln;
+            IssueDate = iDate;
+            IssueType = it;
+            issueDescription = id;
+            issueID = incrementID();
+        }
+        #endregion
+        #region Second Constructor
+        /// <summary>
+        /// Constructor - Point Coordinates, issue type passed as integers and issue description passed as string.
+        /// </summary>
+        /// <param name="coordinateX">Point Value X</param>
+        /// <param name="coordinateY">Point Value Y</param>
+        /// <param name="it">Issue Type</param>
+        /// <param name="id">Issue Description</param>
+        public Tracker(int coordinateX, int coordinateY, int it, string id)
+            : this(coordinateX, coordinateY, it, id, "Not", "Known", 
+                DateTime.Today) { }
+        #endregion
+
+        #region Method - Issue Type
+        /// <summary>
+        /// Determine the issue's type
+        /// </summary>
+        /// <returns>A numeric value representing the issue type</returns>
+        public string GetIssueType()
+        {
+            string typeOutput = "unknown";
+            if (issueType == 1) 
+            { 
+                typeOutput = "facility"; 
+            }
+            if (issueType == 2) 
+            { 
+                typeOutput = "signage"; 
+            }
+            if (issueType == 3) 
+            { 
+                typeOutput = "road"; 
+            }
+            return typeOutput;
+        }
+        #endregion
+        #region Method - Issue details
+        /// <summary>
+        /// Concatenate the issue details.
+        /// </summary>
+        /// <returns>The issue's ID, location, type and description passed as strings</returns>
+        public string GetIssueDetails()
+        {
+            return issueID + ", " + issueLocation + ", "
+                + GetIssueType() + ", " + issueDescription;
+        }
+        /// <summary>
+        /// Concatenate remaining issue fields if the issue date is set.
+        /// </summary>
+        /// <param name="dDate">Include Issue Date</param>
+        /// <returns>All issue details as a string</returns>
+        public string GetIssueDetails(bool iDate)
+        {
+            if (iDate)
+                return GetIssueDetails() + ", " + firstName + " " + lastName 
+                    + ", " + issueDate.ToShortDateString();
+            else
+                return GetIssueDetails();
+        }
+        #endregion
+        #region Method - IncrementID
+        /// <summary>
+        /// Increment the automatically assigned issue ID number.
+        /// </summary>
+        /// <returns>The ID number of the issue.</returns>
+        private static int incrementID()
+        {
+            return CurrentID++;
+        }
+        #endregion
+    }
+}
